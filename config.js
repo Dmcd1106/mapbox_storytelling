99699d1d0d1bf88cbb5ae2e702170a435dffa9b2
Dmcd1106 <Dc.mcdonald2003@gmail.com>
2025-04-22 14:55:21 -0400

    Title change

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Color Purple Essay',
+    title: 'The Color Purple Digital Essay',
     subtitle: 'Spatial Journeys across the South',
     byline: 'My Name',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
